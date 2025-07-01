0fb9bacbead8cad0f9fb98e42404798dbe99dd9c
aopeb <soporteaopeb@gmail.com>
2025-07-01 10:56:47 -0400

    ajuste de modelo

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -12,10 +12,10 @@ const pool = require('../db');
     return res.rows[0];
   }
 
-  async function  create({ name, email,password,role }) {
+  async function  create({ name,lastnamep, lastnamem, ci, birthdate, sex, email, password ,role }) {
     const res = await pool.query(
-      'INSERT INTO users (name, email,password,role) VALUES ($1, $2,$3,$4) RETURNING *',
-      [name, email,password,role]
+      'INSERT INTO users (name,lastnamep, lastnamem, ci, birthdate, sex, email, password ,role) VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
+      [name,lastnamep, lastnamem, ci, birthdate, sex, email, password ,role ]
     );
     return res.rows[0];
   }
@@ -27,12 +27,14 @@ const pool = require('../db');
 
   async function updateById(id, data) {
   // Extraemos los campos que queremos actualizar (por ejemplo name y email)
-  const { name, email ,password, role} = data;
+  const { name,lastnamep, lastnamem, ci, birthdate, sex, email, password ,role} = data;
   console.log ("...........:", data)
   const res = await pool.query(
-    `UPDATE users SET name = $1, email = $2, password = $3 ,role = $4 WHERE id = $5 RETURNING *`,
-    [name, email, password,role,id]
-  );
+   //`UPDATE users SET name = $1, email = $2, password = $3 ,role = $4 WHERE id = $5 RETURNING *`,
+     `UPDATE users SET name = $1,lastnamep =$2, lastnamem=$3, ci=$4, birthdate=$5, sex=$6, email = $7, password = $8 ,role = $9 WHERE id = $10 RETURNING *`,
+   // [name, email, password,role,id]
+    [name,lastnamep, lastnamem, ci, birthdate, sex, email, password ,role,id]
+);
 
   return res.rows[0]; // Retorna el usuario actualizado o undefined si no existe
 }
